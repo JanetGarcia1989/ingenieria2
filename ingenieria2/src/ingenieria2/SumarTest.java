60e75af62f8d3c38823123e5e5d3a690ba18eda3
Debora Mattos <dbymatt@gmail.com>
2018-08-22 20:48:18 -0300

    se agregaron de test

--- ingenieria2/src/ingenieria2/SumarTest.java
+++ ingenieria2/src/ingenieria2/SumarTest.java
@@ -18,6 +18,10 @@ public class SumarTest {
 	@Test
 	public void test2() 
 	{
+		/* 
+		sumo dos numeros, uno positivo y otro negativo
+		resultado nulo
+		*/
 		Calculadora c = new Calculadora ();
 		int resultado = c.sumar(2,-2);
 		int esperado = 0;
@@ -30,6 +34,27 @@ public class SumarTest {
 		Calculadora c = new Calculadora ();
 		int resultado = c.sumar(2,0);
 		int esperado = 2;
+		assertEquals(esperado, resultado);
+	}
+
+	@Test
+	public void test4() 
+	{ /* 
+		sumo dos numeros, uno positivo y otro negativo
+		resultado negativo
+		*/
+		Calculadora c = new Calculadora ();
+		int resultado = c.sumar(2,-3);
+		int esperado = -1;
+		assertEquals(esperado, resultado);
+	}
+	
+	@Test
+	public void test5() 
+	{ //sumo dos numeros negativos
+		Calculadora c = new Calculadora ();
+		int resultado = c.sumar(-2,-3);
+		int esperado = -5;
 		assertEquals(esperado, resultado);
 	}
 }
